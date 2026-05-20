fee5a02e5035d993215e2ef428157c5fc3690c4d
Gabriel Fanado Manfredini <79386399+gab3340@users.noreply.github.com>
2026-05-19 21:02:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach((element) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if (elementTop < windowHeight - revealPoint) {
+            element.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
