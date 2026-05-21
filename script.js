d681ae3a34b2e600fc062cbefaa9ec65db9e93c0
Gabriel Fanado Manfredini <79386399+gab3340@users.noreply.github.com>
2026-05-21 20:55:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
+let projectIndex = 0;
+
+const projects = document.querySelectorAll(".projects-carousel .project-card");
+const dots = document.querySelectorAll(".dot");
+
+function showProject(index) {
+    if (index >= projects.length) {
+        projectIndex = 0;
+    }
+
+    if (index < 0) {
+        projectIndex = projects.length - 1;
+    }
+
+    projects.forEach((project) => {
+        project.classList.remove("active");
+    });
+
+    dots.forEach((dot) => {
+        dot.classList.remove("active");
+    });
+
+    projects[projectIndex].classList.add("active");
+    dots[projectIndex].classList.add("active");
+}
+
+function changeProject(direction) {
+    projectIndex = projectIndex + direction;
+    showProject(projectIndex);
+}
+
+function currentProject(index) {
+    projectIndex = index;
+    showProject(projectIndex);
+}
+
+showProject(projectIndex);
